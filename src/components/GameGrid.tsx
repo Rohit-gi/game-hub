9a0c84e969baf7e2463654d87fc4266df5c9716b
Rohit <i.rohit003@gmail.com>
2023-03-30 21:44:34 +0530

    Refactor commit

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -10,6 +10,7 @@ import useGames from "../hooks/useGames";
 import GameCard from "./GameCard";
 import { Game } from "../hooks/useGames";
 import GameCardSkeleton from "./GameCardSkeleton";
+import GameCardContainer from "./GameCardContainer";
 
 const GameGrid = () => {
   const { games, error, isLoading } = useGames();
@@ -23,23 +24,17 @@ const GameGrid = () => {
         padding="10px"
         spacing={10}
       >
-        {isLoading && skeletons.map(skeleton => GameCardSkeleton key={skeletons})}
-        {games.map(
-          (game: {
-            id: Key | null | undefined;
-            name:
-              | string
-              | number
-              | boolean
-              | ReactElement<any, string | JSXElementConstructor<any>>
-              | ReactFragment
-              | ReactPortal
-              | null
-              | undefined;
-          }) => (
+        {isLoading &&
+          skeletons.map((skeleton) => (
+            <GameCardContainer>
+              <GameCardSkeleton key={skeleton} />
+            </GameCardContainer>
+          ))}
+        {games.map((game) => (
+          <GameCardContainer>
             <GameCard key={game.id} game={game} />
-          )
-        )}
+          </GameCardContainer>
+        ))}
       </SimpleGrid>
     </>
   );
